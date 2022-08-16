9a0ad75a430c1222f74f7b775f477cedb1ed4842
Pablo Figueroa <pablofigue03@gmail.com>
2022-08-16 19:21:49 -0300

    first commit

--- js/main.js
+++ js/main.js
@@ -8,7 +8,49 @@ function buscarJuego() {
             x[i].style.display="none";
         }
         else {
-            x[i].style.display="list-item";                 
+            x[i].style.display="";                 
         }
     }
 }
+
+function filtroSony() {
+    
+    let games = document.getElementsByClassName('game_content')
+    const isChecked = event.target.checked
+
+    if(isChecked) {
+        for (let i = 0; i < games.length; i++) {
+            if (games[i].className.includes('onlyplay')) {
+                games[i].style.display = ''
+            } else {
+                games[i].style.display = 'none'
+            }
+        } 
+    }else {
+        for(let i = 0; i < games.length; i++) {
+            games[i].style.display = ''
+        }
+    }
+}
+
+function filtroXbox() {
+    
+    let games = document.getElementsByClassName('game_content')
+    const isChecked = event.target.checked
+
+    if(isChecked) {
+        for (let i = 0; i < games.length; i++) {
+            if (games[i].className.includes('onlyxbox')) {
+                games[i].style.display = ''
+            } else {
+                games[i].style.display = 'none'
+            }
+        } 
+    }else {
+        for(let i = 0; i < games.length; i++) {
+            games[i].style.display = ''
+        }
+    }
+}
+
+
